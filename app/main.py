84d7b879eaf72969f7c34f15cb4bb0f0b8939ceb
Arafat Eajaz <arafat@arafat-macbookpro.roam.corp.google.com>
2015-10-14 17:46:57 +0530

    .

--- app/main.py
+++ app/main.py
@@ -5,12 +5,12 @@ from webapp2_extras import jinja2
 class MainPage(webapp2.RequestHandler):
 
   @webapp2.cached_property
-    def jinja2(self):
-      # Returns a Jinja2 renderer cached in the app registry.
-      return jinja2.get_jinja2(app=self.app)
+  def jinja2(self):
+    # Returns a Jinja2 renderer cached in the app registry.
+    return jinja2.get_jinja2(app=self.app)
 
   def get(self):
-    rv = self.jinja2.render_template('index.html')
+    rv = self.jinja2.render_template('templates/index.html')
     self.response.write(rv)
     
 app = webapp2.WSGIApplication([('/', MainPage),], debug=True)
