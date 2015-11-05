19cb2b138ac8818371bfef0e0a2904ce815d0d91
Arafat Eajaz <arafat@google.com>
2015-11-05 19:26:20 +0530

    .

---
+++ chetanya-wedding/README.md
@@ -0,0 +1,5 @@
+# chetanya-wedding
+Website for chetanya wedding
+
+# Update command.
+appcfg.py -A chetanya-meghashree update app/app.yaml
---
+++ chetanya-wedding/app/main.py
@@ -0,0 +1,16 @@
+import webapp2
+
+from webapp2_extras import jinja2
+
+class MainPage(webapp2.RequestHandler):
+
+  @webapp2.cached_property
+  def jinja2(self):
+    # Returns a Jinja2 renderer cached in the app registry.
+    return jinja2.get_jinja2(app=self.app)
+
+  def get(self):
+    rv = self.jinja2.render_template('new/index.html')
+    self.response.write(rv)
+
+app = webapp2.WSGIApplication([('/', MainPage),], debug=True)
